8ae8429a4008d3ccb7d09b69f6db0abaacb96dae
S-kwen <61229819+s-kwen@users.noreply.github.com>
2021-05-04 03:48:43 +0800

    implement-strstr

---
+++ src/main/java/com/boot/leetcode/implementstrstr/ImplementStrStr.java
@@ -0,0 +1,31 @@
+package com.boot.leetcode.implementstrstr;
+
+/**
+ * @PackageName: com.boot.leetcode.implementstrstr
+ * @ClassName: ImplementStrStr
+ * @Description: This is ImplementStrStr class by Skwen.
+ * @Author: Skwen
+ * @Date: 2021-05-04 3:47
+ */
+public class ImplementStrStr {
+    public static void main(String[] args) {
+        System.out.println(strStr("hello","ll"));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        for (int i = 0; i + m <= n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
