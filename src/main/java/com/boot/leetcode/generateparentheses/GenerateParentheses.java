6dff0180190806664bc6b604dc150b271147fb3c
S-kwen <61229819+s-kwen@users.noreply.github.com>
2021-05-06 22:58:21 +0800

    generate-parentheses

---
+++ src/main/java/com/boot/leetcode/generateparentheses/GenerateParentheses.java
@@ -0,0 +1,52 @@
+package com.boot.leetcode.generateparentheses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @PackageName: com.boot.leetcode.generateparentheses
+ * @ClassName: GenerateParentheses
+ * @Description: This is GenerateParentheses class by Skwen.
+ * @Author: Skwen
+ * @Date: 2021-05-06 22:56
+ */
+public class GenerateParentheses {
+    public static void main(String[] args) {
+        System.out.println(generateParenthesis(3));
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> combinations = new ArrayList<String>();
+        generateAll(new char[2 * n], 0, combinations);
+        return combinations;
+    }
+
+    public static void generateAll(char[] current, int pos, List<String> result) {
+        if (pos == current.length) {
+            if (valid(current)) {
+                result.add(new String(current));
+            }
+        } else {
+            current[pos] = '(';
+            generateAll(current, pos + 1, result);
+            current[pos] = ')';
+            generateAll(current, pos + 1, result);
+        }
+    }
+
+    public static boolean valid(char[] current) {
+        int balance = 0;
+        for (char c : current) {
+            if (c == '(') {
+                ++balance;
+            } else {
+                --balance;
+            }
+            if (balance < 0) {
+                return false;
+            }
+        }
+        return balance == 0;
+    }
+
+}
