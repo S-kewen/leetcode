244a91e2b6d1c5bf8ee5efd1bed2198829d5fc9a
S-kwen <61229819+s-kwen@users.noreply.github.com>
2021-05-10 02:54:31 +0800

    valid-phone-numbers

---
+++ src/main/java/com/boot/leetcode/validphonenumbers/ValidPhoneNumbers.java
@@ -0,0 +1,14 @@
+package com.boot.leetcode.validphonenumbers;
+
+/**
+ * @PackageName: com.boot.leetcode.validphonenumbers
+ * @ClassName: ValidPhoneNumbers
+ * @Description: This is ValidPhoneNumbers class by Skwen.
+ * @Author: Skwen
+ * @Date: 2021-05-10 2:52
+ */
+public class ValidPhoneNumbers {
+    public static void main(String[] args) {
+//        grep -P "^\d{3}-\d{3}-\d{4}$|^(\d{3})\s\d{3}-\d{4}$" file.txt
+    }
+}
