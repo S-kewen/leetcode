ada641d1ecb1687b11bf32682e18f4198efda733
S-kwen <61229819+s-kwen@users.noreply.github.com>
2021-05-11 00:57:34 +0800

    rising-temperature

---
+++ src/main/java/com/boot/leetcode/risingtemperature/RisingTemperature.java
@@ -0,0 +1,21 @@
+package com.boot.leetcode.risingtemperature;
+
+/**
+ * @PackageName: com.boot.leetcode.risingtemperature
+ * @ClassName: RisingTemperature
+ * @Description: This is RisingTemperature class by Skwen.
+ * @Author: Skwen
+ * @Date: 2021-05-11 0:57
+ */
+public class RisingTemperature {
+    public static void main(String[] args) {
+//        SELECT
+//        weather.id AS 'Id'
+//        FROM
+//                weather
+//        JOIN
+//        weather w ON DATEDIFF(weather.date, w.date) = 1
+//        AND weather.Temperature > w.Temperature
+//        ;
+    }
+}
