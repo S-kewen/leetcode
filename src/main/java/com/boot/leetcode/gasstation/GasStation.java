f0eda5119fbd36a8a6652dac4810835c1f6ebb06
S-kwen <61229819+s-kwen@users.noreply.github.com>
2021-05-25 03:24:59 +0800

    gas-station

---
+++ src/main/java/com/boot/leetcode/gasstation/GasStation.java
@@ -0,0 +1,38 @@
+package com.boot.leetcode.gasstation;
+
+/**
+ * @PackageName: com.boot.leetcode.gasstation
+ * @ClassName: GasStation
+ * @Description: This is GasStation class by Skwen.
+ * @Author: Skwen
+ * @Date: 2021-05-25 3:23
+ */
+public class GasStation {
+    public static void main(String[] args) {
+        System.out.println(canCompleteCircuit(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 5, 1, 2}));
+    }
+
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        int i = 0;
+        while (i < n) {
+            int sumOfGas = 0, sumOfCost = 0;
+            int cnt = 0;
+            while (cnt < n) {
+                int j = (i + cnt) % n;
+                sumOfGas += gas[j];
+                sumOfCost += cost[j];
+                if (sumOfCost > sumOfGas) {
+                    break;
+                }
+                cnt++;
+            }
+            if (cnt == n) {
+                return i;
+            } else {
+                i = i + cnt + 1;
+            }
+        }
+        return -1;
+    }
+}
