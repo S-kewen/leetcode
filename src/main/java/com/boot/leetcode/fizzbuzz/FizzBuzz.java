8225945c2649df559ccc8b9bac5b2b5ac7a86ca7
S-kwen <61229819+s-kwen@users.noreply.github.com>
2021-05-30 03:47:31 +0800

    fizz-buzz

---
+++ src/main/java/com/boot/leetcode/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,44 @@
+package com.boot.leetcode.fizzbuzz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @PackageName: com.boot.leetcode.fizzbuzz
+ * @ClassName: FizzBuzz
+ * @Description: This is FizzBuzz class by Skwen.
+ * @Author: Skwen
+ * @Date: 2021-05-30 3:46
+ */
+public class FizzBuzz {
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(1));
+    }
+    public static List<String> fizzBuzz(int n) {
+
+        // ans list
+        List<String> ans = new ArrayList<String>();
+
+        for (int num = 1; num <= n; num++) {
+
+            boolean divisibleBy3 = (num % 3 == 0);
+            boolean divisibleBy5 = (num % 5 == 0);
+
+            if (divisibleBy3 && divisibleBy5) {
+                // Divides by both 3 and 5, add FizzBuzz
+                ans.add("FizzBuzz");
+            } else if (divisibleBy3) {
+                // Divides by 3, add Fizz
+                ans.add("Fizz");
+            } else if (divisibleBy5) {
+                // Divides by 5, add Buzz
+                ans.add("Buzz");
+            } else {
+                // Not divisible by 3 or 5, add the number
+                ans.add(Integer.toString(num));
+            }
+        }
+
+        return ans;
+    }
+}
