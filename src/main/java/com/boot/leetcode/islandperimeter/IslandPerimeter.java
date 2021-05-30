19b092b4dcd669f74908caf45b64d88372220913
S-kwen <61229819+s-kwen@users.noreply.github.com>
2021-05-31 02:07:40 +0800

    island-perimeter

---
+++ src/main/java/com/boot/leetcode/islandperimeter/IslandPerimeter.java
@@ -0,0 +1,39 @@
+package com.boot.leetcode.islandperimeter;
+
+/**
+ * @PackageName: com.boot.leetcode.islandperimeter
+ * @ClassName: IslandPerimeter
+ * @Description: This is IslandPerimeter class by Skwen.
+ * @Author: Skwen
+ * @Date: 2021-05-31 2:06
+ */
+public class IslandPerimeter {
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+    public static void main(String[] args) {
+        System.out.println(islandPerimeter(new int[][]{{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}}));
+    }
+
+
+    public static int islandPerimeter(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 1) {
+                    int cnt = 0;
+                    for (int k = 0; k < 4; ++k) {
+                        int tx = i + dx[k];
+                        int ty = j + dy[k];
+                        if (tx < 0 || tx >= n || ty < 0 || ty >= m || grid[tx][ty] == 0) {
+                            cnt += 1;
+                        }
+                    }
+                    ans += cnt;
+                }
+            }
+        }
+        return ans;
+    }
+}
