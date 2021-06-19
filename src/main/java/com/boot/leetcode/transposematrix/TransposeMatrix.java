fa5e0115457ece3bddce1892cf09fbd3b8542667
S-kwen <61229819+s-kwen@users.noreply.github.com>
2021-06-19 21:04:05 +0800

    transpose-matrix

---
+++ src/main/java/com/boot/leetcode/transposematrix/TransposeMatrix.java
@@ -0,0 +1,21 @@
+package com.boot.leetcode.transposematrix;
+
+/**
+ * @PackageName: com.boot.leetcode.transposematrix
+ * @ClassName: TransposeMatrix
+ * @Description: This is TransposeMatrix class by Skwen.
+ * @Author: Skwen
+ * @Date: 2021-06-19 21:03
+ */
+public class TransposeMatrix {
+    public static int[][] transpose(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[][] transposed = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+}
