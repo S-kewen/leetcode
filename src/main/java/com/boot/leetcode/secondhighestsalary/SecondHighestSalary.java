9400ad1a0b76e85569765300f451a9c51373b1ba
S-kwen <61229819+s-kwen@users.noreply.github.com>
2021-06-25 03:23:10 +0800

    second-highest-salary

---
+++ src/main/java/com/boot/leetcode/secondhighestsalary/SecondHighestSalary.java
@@ -0,0 +1,20 @@
+package com.boot.leetcode.secondhighestsalary;
+
+/**
+ * @PackageName: com.boot.leetcode.secondhighestsalary
+ * @ClassName: SecondHighestSalary
+ * @Description: This is SecondHighestSalary class by Skwen.
+ * @Author: Skwen
+ * @Date: 2021-06-25 3:22
+ */
+public class SecondHighestSalary {
+    public static void main(String[] args) {
+//        SELECT
+//                (SELECT DISTINCT
+//                        Salary
+//                        FROM
+//                        Employee
+//                        ORDER BY Salary DESC
+//                        LIMIT 1 OFFSET 1) AS SecondHighestSalary
+    }
+}
